136c2b31fcdae647fa54faf039a9a72cff7d0cfa
Martina <marth.tasevska@gmail.com>
2021-08-12 17:53:26 +0200

    root to greetings' index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "greetings#index"
+  resources :greetings
   resources :greetings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
